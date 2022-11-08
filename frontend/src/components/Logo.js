fba8e81c3bde87af6b1732f0ecb08d59652c9ea3
Silvasss <felipe_silva55@hotmail.com>
2022-11-08 15:08:25 -0300

    update

--- frontend/src/components/Logo.js
+++
@@ -1,9 +0,0 @@
-import logo from '../assets/images/logo2.svg'
-
-
-const Logo = () => {
-    return <img src={logo} alt='Alumni' className='logo' width='10%'/>
-}
-
-
-export default Logo
--- frontend/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Landing from "./landing";
-import Error from "./Error";
-import Register from "./Register";
-import ProtectedRoute from "./ProtectedRoute";
-
-
-export {Landing, Error, Register, ProtectedRoute}
