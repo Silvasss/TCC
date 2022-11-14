1080dede97ff4f25170cc7fdc7d1017833cc5a76
Silvasss <felipe_silva55@hotmail.com>
2022-11-14 10:50:50 -0300

    -.4

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 app.use(express.json())
 app.use(helmet())
