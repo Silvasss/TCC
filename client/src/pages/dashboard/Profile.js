119506eb5914adebc4c2e449c705725b13470a48
Silvasss <felipe_silva55@hotmail.com>
2023-03-23 16:59:02 -0300

    campos de entrada de seleção país, estado e cidade

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import { City, Country, State } from "country-state-city"
 
 import { FormRow, Alert, JobsContainer, FormRowSelect } from '../../components'
 import { useAppContext } from '../../context/appContext'
 import Wrapper from '../../assets/wrappers/DashboardFormPage'
+import Select from 'react-select'
 
 
 const Profile = () => {
   const { user, showAlert, displayAlert, updateUser, isLoading, gradLocation } = useAppContext()
+
+  let countryData = Country.getAllCountries()
 
   const [name, setName] = useState(user?.name)
   const [email, setEmail] = useState(user?.email)
@@ -23,6 +28,23 @@ const Profile = () => {
 
     updateUser({ name, email, location })
   }
+
+  const [stateData, setStateData] = useState()
+  const [cityData, setCityData] = useState()
+
+  const [country, setCountry] = useState(countryData[0])
+  const [state, setState] = useState()
+  const [city, setCity] = useState()
+
+  useEffect(() => {setStateData(State.getStatesOfCountry(country?.isoCode))}, [country])
+
+  useEffect(() => {setCityData(City.getCitiesOfState(country?.isoCode, state?.isoCode))}, [state])
+
+  useEffect(() => {stateData && setState(stateData[0])}, [stateData])
+
+  useEffect(() => {cityData && setCity(cityData[0])}, [cityData])
+
+  //console.log(countryData)
 
   return (
     <Wrapper>
@@ -48,12 +70,36 @@ const Profile = () => {
           />
           
           <FormRowSelect
-            name='location' 
+            name='Localização' 
             value={location} 
             handleChange={(e) => setLocation(e[1])} 
             list={gradLocation}
             selectValue={location}
           />
+
+          <Select name={name} 
+            placeholder='Selecione um País'
+            options={countryData}  
+            getOptionLabel={(option) => option.name}
+            getOptionValue={(option) => option.name}
+            onChange={setCountry}
+          />     
+          
+          <Select name={name} 
+            placeholder='Selecione o Estado'
+            options={stateData}  
+            getOptionLabel={(option) => option.name}
+            getOptionValue={(option) => option.name}
+            onChange={setState}
+          />
+          
+          <Select name={name} 
+            placeholder='Selecione a Cidade'
+            options={cityData}  
+            getOptionLabel={(option) => option.name}
+            getOptionValue={(option) => option.name}
+            onChange={setCity}
+          /> 
 
           <button className='btn btn-block' type='submit' disabled={isLoading}>
             {isLoading ? 'Por favor, aguarde...' : 'salvar alterações'}
