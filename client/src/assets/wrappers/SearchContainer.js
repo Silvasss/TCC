38a1c2fec1ab59bfc24c3bd3f4d29837c46ae708
Silvasss <felipe_silva55@hotmail.com>
2023-04-17 16:10:16 -0300

    posição do botão "limpar filtros"

--- client/src/assets/wrappers/SearchContainer.js
+++ client/src/assets/wrappers/SearchContainer.js
@@ -38,45 +38,73 @@ const Wrapper = styled.section`
 
   .searc-btn {
     background-color: #57abff;
+
     border-radius: 0.25rem;
+
     border: transparent;
+
     display: inline-block;
+
     cursor: pointer;
+
     padding: 0.375rem 0.75rem;
+
     letter-spacing: 1px;
+
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
     transition: 0.3s ease-in-out all;
   }
 
   .searc-btn:hover {
     background-color:#80b5ea;
+
     color: #fff;
   }
 
   .searc-btn:active {
-    position:relative;
+    position: relative;
+
     top:1px;
   }
 
   .limpar-btn {
     background-color: #c25959;
+
+    position: relative;
+        
+    left: 273%;
+
+    top: 30%;
+
+    width: 50%;
+
     border-radius: 0.25rem;
+
     border: transparent;
+
     display: inline-block;
+
     cursor: pointer;
+
     padding: 0.375rem 0.75rem;
+
     letter-spacing: 1px;
+
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
     transition: 0.3s ease-in-out all;
   }
 
   .limpar-btn:hover {
     background-color:#f09e90;
+
     color: #fff;
   }
 
   .limpar-btn:active {
     position:relative;
+
     top:1px;
   }
 
